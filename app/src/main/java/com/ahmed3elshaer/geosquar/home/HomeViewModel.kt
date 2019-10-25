f9a965b9a5d9aa4e950fcad69bf5d3467c09670a
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-10-25 23:06:56 +0200

    wrapping viewstate in event

--- app/src/main/java/com/ahmed3elshaer/geosquar/home/HomeViewModel.kt
+++ app/src/main/java/com/ahmed3elshaer/geosquar/home/HomeViewModel.kt
@@ -2,7 +2,8 @@ package com.ahmed3elshaer.geosquar.home
 
 import androidx.lifecycle.MutableLiveData
 import com.ahmed3elshaer.geosquar.common.BaseViewModel
+import com.ahmed3elshaer.geosquar.common.Event
 
-class HomeViewModel() : BaseViewModel<HomeViewState>() {
-    override val _viewState = MutableLiveData<HomeViewState>()
+class HomeViewModel() : BaseViewModel<Event<HomeViewState>>() {
+    override val _viewState = MutableLiveData<Event<HomeViewState>>()
 }
