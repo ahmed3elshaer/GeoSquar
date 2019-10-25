fd3ad964fce64a5f7bf6a80bc8e467890c19a993
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-10-25 23:47:20 +0200

    adding last accepted location caching

--- app/src/main/java/com/ahmed3elshaer/geosquar/common/Repository.kt
+++ app/src/main/java/com/ahmed3elshaer/geosquar/common/Repository.kt
@@ -1,5 +1,6 @@
 package com.ahmed3elshaer.geosquar.common
 
+import com.ahmed3elshaer.geosquar.common.SharedPrefWrapper.Companion.LOCATION_KEY
 import com.ahmed3elshaer.geosquar.common.models.VenuesRequest
 
 class Repository(
@@ -8,9 +9,9 @@ class Repository(
 ) {
 
     fun exploreVenues(venuesRequest: VenuesRequest) = fourSquareApi.exploreVenues(venuesRequest)
-
     fun getVenueImages(venueId: String) = fourSquareApi.getVenuePhotos(venueId)
-
     fun getPhotoStream(url: String) = fourSquareApi.getPhotoStream(url)
+    fun cacheLocation(coordinates: String) = sharedPrefWrapper.saveString(LOCATION_KEY, coordinates)
+    fun getCachedLocation() = sharedPrefWrapper.getString(LOCATION_KEY)
 
 }
