44cd5dd6429fdcf97fd6b06641e3a5c4fe835f04
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-10-26 05:33:29 +0200

    setting default radius to 1000m

--- app/src/main/java/com/ahmed3elshaer/geosquar/common/models/VenuesRequest.kt
+++ app/src/main/java/com/ahmed3elshaer/geosquar/common/models/VenuesRequest.kt
@@ -1,6 +1,14 @@
+/*
+ * *
+ *  * Created by Ahmed Elshaer on 10/26/19 4:17 AM
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 10/25/19 12:27 AM
+ *
+ */
+
 package com.ahmed3elshaer.geosquar.common.models
 
 import com.squareup.moshi.Json
 import retrofit2.http.Query
 
-data class VenuesRequest(@Query("ll") val coordinates: String, @Query("radius") val radius: Long)
+data class VenuesRequest(@Query("ll") val coordinates: String, @Query("radius") val radius: Long = 1000)
