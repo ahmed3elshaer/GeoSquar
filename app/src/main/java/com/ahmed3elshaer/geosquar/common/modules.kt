08b2e4b6a67602452edb95535a05d6dd4ddc2885
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-10-26 05:33:48 +0200

    providing usecases and viewmodel

--- app/src/main/java/com/ahmed3elshaer/geosquar/common/modules.kt
+++ app/src/main/java/com/ahmed3elshaer/geosquar/common/modules.kt
@@ -1,13 +1,26 @@
+/*
+ * *
+ *  * Created by Ahmed Elshaer on 10/26/19 4:17 AM
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 10/26/19 3:34 AM
+ *
+ */
+
 package com.ahmed3elshaer.geosquar.common
 
 import android.content.Context
 import com.ahmed3elshaer.geosquar.common.local.VenuesDatabase
+import com.ahmed3elshaer.geosquar.home.HomeViewModel
+import com.ahmed3elshaer.geosquar.home.usecases.ExploreVenuesCacheUseCase
+import com.ahmed3elshaer.geosquar.home.usecases.ExploreVenuesRealtimeUseCase
+import com.ahmed3elshaer.geosquar.home.usecases.ExploreVenuesSingleUseCase
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -30,7 +43,13 @@ val applicationModules = module {
         )
 
     }
-    factory { Repository(get(), get(),get()) }
+    factory { Repository(get(), get(), get()) }
+
+    factory { ExploreVenuesCacheUseCase(get()) }
+    factory { ExploreVenuesRealtimeUseCase(get()) }
+    factory { ExploreVenuesSingleUseCase(get()) }
+
+    viewModel { HomeViewModel(get(), get(), get()) }
 }
 
 fun createOkHttpClient(): OkHttpClient {
