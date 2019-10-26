ed0217f68deb406da22b3a06e8069742407bb97f
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-10-26 05:30:08 +0200

    adding requests auth for foursquare apis

--- app/src/main/java/com/ahmed3elshaer/geosquar/common/AuthInterceptor.kt
+++ app/src/main/java/com/ahmed3elshaer/geosquar/common/AuthInterceptor.kt
@@ -1,3 +1,11 @@
+/*
+ * *
+ *  * Created by Ahmed Elshaer on 10/26/19 4:17 AM
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 10/24/19 9:31 PM
+ *
+ */
+
 package com.ahmed3elshaer.geosquar.common
 
 import okhttp3.Interceptor
