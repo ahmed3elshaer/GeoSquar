57080b0d4bcf41bdd07568f85f973cd51b227c14
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-10-26 05:35:11 +0200

    injecting usecases

--- app/src/main/java/com/ahmed3elshaer/geosquar/home/HomeViewModel.kt
+++ app/src/main/java/com/ahmed3elshaer/geosquar/home/HomeViewModel.kt
@@ -1,14 +1,31 @@
+/*
+ * *
+ *  * Created by Ahmed Elshaer on 10/26/19 4:17 AM
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 10/26/19 3:37 AM
+ *
+ */
+
 package com.ahmed3elshaer.geosquar.home
 
+import android.location.Location
 import androidx.lifecycle.MutableLiveData
 import com.ahmed3elshaer.geosquar.common.BaseViewModel
 import com.ahmed3elshaer.geosquar.common.Event
+import com.ahmed3elshaer.geosquar.common.models.VenuesRequest
+import com.ahmed3elshaer.geosquar.home.usecases.ExploreVenuesCacheUseCase
+import com.ahmed3elshaer.geosquar.home.usecases.ExploreVenuesRealtimeUseCase
+import com.ahmed3elshaer.geosquar.home.usecases.ExploreVenuesSingleUseCase
 
 class HomeViewModel(
-    private val exploreVenuesRealtimeUseCase: ExploreVenuesRealtimeUseCase,
-    private val exploreVenuesSingleUseCase: ExploreVenuesSingleUseCase,
-    private val exploreVenuesCacheUseCase: ExploreVenuesCacheUseCase
-) : BaseViewModel<Event<HomeViewState>>() {
+        private val exploreVenuesRealtimeUseCase: ExploreVenuesRealtimeUseCase,
+        private val exploreVenuesSingleUseCase: ExploreVenuesSingleUseCase,
+        private val exploreVenuesCacheUseCase: ExploreVenuesCacheUseCase
+) : BaseViewModel<HomeViewState>() {
+    init {
+        post(HomeViewState())
+    }
+
     fun checkForCachedVenues() {
         add {
             exploreVenuesCacheUseCase()
@@ -56,5 +73,6 @@ class HomeViewModel(
                     })
         }
     }
+
     override val _viewState = MutableLiveData<Event<HomeViewState>>()
 }
