c02412fe2516f2415b775a55ed36e7b2d37e91e4
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-10-26 05:35:27 +0200

    adding error state

--- app/src/main/java/com/ahmed3elshaer/geosquar/home/HomeViewState.kt
+++ app/src/main/java/com/ahmed3elshaer/geosquar/home/HomeViewState.kt
@@ -1,9 +1,17 @@
+/*
+ * *
+ *  * Created by Ahmed Elshaer on 10/26/19 4:17 AM
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 10/26/19 3:54 AM
+ *
+ */
+
 package com.ahmed3elshaer.geosquar.home
 
 import com.ahmed3elshaer.geosquar.common.models.Venue
-import com.ahmed3elshaer.geosquar.common.models.VenuesResponse
 
 data class HomeViewState(
-    val isLoading: Boolean = false,
-    val venues: List<Venue>
+        val isLoading: Boolean = false,
+        val venues: List<Venue>? = null,
+        val error: Throwable? = null
 )
