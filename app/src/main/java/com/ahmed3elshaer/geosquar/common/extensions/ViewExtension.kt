8e05ba4514182493831d5ee26fdf766b5fe0d9cc
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-10-26 09:23:12 +0200

    visibility extensions

---
+++ app/src/main/java/com/ahmed3elshaer/geosquar/common/extensions/ViewExtension.kt
@@ -0,0 +1,20 @@
+/*
+ * *
+ *  * Created by Ahmed Elshaer on 10/26/19 5:47 AM
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 10/26/19 5:47 AM
+ *
+ */
+
+package com.ahmed3elshaer.geosquar.common.extensions
+
+import android.view.View
+
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+
+fun View.hide() {
+    this.visibility = View.GONE
+}
